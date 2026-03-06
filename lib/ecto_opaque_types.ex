0e3e3472b08220a5c6ed581b17ba48f88aa2fe9f
Tobias Pfeiffer <tpfeiffer@together.ai>
2026-03-06 13:40:39 +0100

    Initial empty repo

---
+++ lib/ecto_opaque_types.ex
@@ -0,0 +1,18 @@
+defmodule EctoOpaqueTypes do
+  @moduledoc """
+  Documentation for `EctoOpaqueTypes`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoOpaqueTypes.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_opaque_types_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoOpaqueTypesTest do
+  use ExUnit.Case
+  doctest EctoOpaqueTypes
+
+  test "greets the world" do
+    assert EctoOpaqueTypes.hello() == :world
+  end
+end
